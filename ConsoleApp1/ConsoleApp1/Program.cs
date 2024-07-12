ac5e8e19ea085ff82063ad4a56b2a73b2f0f1884
Charles Pereira <charlespereira.fln@gmail.com>
2024-07-12 10:23:26 -0300

    variaveis

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,28 @@ namespace ConsoleApp3 // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // TipoDessaVariavel nomeParaEssaVariavel = atribuirUmValor
+
+            // int - armazenam números inteiros
+            int numero1 = 10;
+
+            // float - armazenam números quebrados
+            float numero2 = 2.5f;
+
+            // string - armazena um texto
+            string nome = "Charles";
+
+            // char - armazena um caractere
+            char sexo = 'M';
+
+            Console.WriteLine(numero1);
+            Console.WriteLine(sexo);
+            Console.WriteLine(nome);
+            Console.WriteLine(numero2);
+      
+
+            // bool - verdadeiro ou falso
+
         }
     }
 }
