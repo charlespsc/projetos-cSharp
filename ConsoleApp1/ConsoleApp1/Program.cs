43dc22e9c8986a7c255c384ac07ed84627e05028
Charles Pereira <charlespereira.fln@gmail.com>
2024-07-12 10:37:40 -0300

    operadoresAritmeticos

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -24,9 +24,32 @@ namespace ConsoleApp3 // Note: actual namespace depends on the project name.
             Console.WriteLine(sexo);
             Console.WriteLine(nome);
             Console.WriteLine(numero2);
-      
+
 
             // bool - verdadeiro ou falso
+
+            // Operadores Aritméticos
+
+            int soma = 5 + 5;
+            Console.WriteLine(soma);
+
+            int num1 = 5;
+            int num2 = 10;
+
+            int total = (num1 + num2);
+            Console.WriteLine(total);
+
+            Console.WriteLine("Escreva um número");
+            int numero_2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escreva outro número");
+            int numero_3 = Convert.ToInt32(Console.ReadLine());
+
+            int totalN = (numero_2 + numero_3);
+
+            Console.WriteLine("O Total é: " + totalN);
+
+
 
         }
     }
