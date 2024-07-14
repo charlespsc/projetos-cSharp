6d90b2c38257ae1d0ec53fdcee996042bd117511
charlespsc <charlesp.sc@outlook.com>
2024-07-14 19:58:41 -0300

    while_for

---
+++ LacosRepeticao/LacosRepeticao/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Estrutura de Repetição
+  
+        // Foreach
+
+        // Do (while)
+
+        // While
+        int numero = 0;
+
+        while (numero <= 10)
+        {
+            Console.WriteLine(numero);
+            numero++;
+        }
+
+        // For
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+}
