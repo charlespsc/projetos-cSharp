bd7f9816044249d0591f2a3863b8346de3d357e4
charlespsc <charlesp.sc@outlook.com>
2024-07-14 21:31:12 -0300

    classesConstrutor

---
+++ ConsoleAppClasses/ConsoleAppClasses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleAppClasses
+{
+    public class Aluno
+    {
+        public string nome = "Charles";
+        public int nota_tr1 = 10;
+        public long cpf = 11122233344;
+    }
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            Aluno aluno1 = new Aluno();
+
+            Console.WriteLine(aluno.nome);
+
+            Aluno tr1 = new Aluno();
+            Console.WriteLine(tr1.nota_tr1);
+        }
+    }
+}
---
+++ ConsoleAppConstrutor/ConsoleAppConstrutor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleAppConstrutor
+{
+    public class Aluno
+    {
+        public Aluno(string nome)
+        {
+            nomeDoAluno = nome;
+        }
+        public string nomeDoAluno { get; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno1 = new Aluno("Charles");
+            Console.WriteLine(aluno1.nomeDoAluno);
+
+            Aluno aluno2 = new Aluno("Luan");
+            Console.WriteLine(aluno2.nomeDoAluno);
+
+        }
+    }
+}
+
