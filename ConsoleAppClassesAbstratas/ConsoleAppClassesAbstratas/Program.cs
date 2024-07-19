7529d49b1d52b2fb5922001ccdb2f7ee38217c78
charlespsc <charlesp.sc@outlook.com>
2024-07-19 18:02:16 -0300

    Classes Abstratas + Override

---
+++ ConsoleAppClassesAbstratas/ConsoleAppClassesAbstratas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleAppClassesAbstratas
+{
+    abstract class Forma
+    {
+        public abstract int CalcularArea();
+    }
+
+    class Quadrado : Forma
+    {
+        int lado;
+        public Quadrado(int n) => lado = n;
+
+        public override int CalcularArea() => lado * lado;
+    }
+       
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            var qd = new Quadrado(12);
+
+            Console.WriteLine($"Área do quadrando = {qd.CalcularArea()}");
+        }
+    }
+}
