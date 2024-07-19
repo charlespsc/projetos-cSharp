a5467f486deb0f5fb1991458851f195a59de64bf
charlespsc <charlesp.sc@outlook.com>
2024-07-19 17:07:41 -0300

    Métodos

---
+++ ConsoleAppMetodos/ConsoleAppMetodos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleAppMetodos
+{
+    abstract class Motocicleta
+    {
+        // Qualquer um consegue chamar este methodo 
+        public void LigarMoto() { }
+
+        // Somente classes derivadas conseguem chamar (heranças)
+        protected void AdicionarGasolina(int galoes) { }
+
+        public abstract double ChegarNaVelocidadeX();
+
+        public virtual int Drive(int quilometros, int velocidade) { }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
