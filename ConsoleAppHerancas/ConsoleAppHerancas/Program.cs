39319104c75db11196bc3af70c5c300ab7b98dc3
charlespsc <charlesp.sc@outlook.com>
2024-07-19 17:46:09 -0300

    Heranças

---
+++ ConsoleAppHerancas/ConsoleAppHerancas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleAppHerancas
+{
+    public class Alessandra
+    {
+        private int valor = 10;
+
+        public class Bruno : Alessandra
+        {
+            public int PegarValor()
+            {
+            return this.valor;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Bruno = new Alessandra.Bruno();
+            Console.WriteLine(Bruno.PegarValor());
+        }
+    }
+}
