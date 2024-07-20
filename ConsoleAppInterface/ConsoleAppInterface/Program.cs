d920efae329d78f87d60406e194432507d9fed99
charlespsc <charlesp.sc@outlook.com>
2024-07-20 03:29:25 -0300

    Interface

---
+++ ConsoleAppInterface/ConsoleAppInterface/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleAppInterface
+{
+    interface IControle
+    {
+        void ApertarBotao();
+    }
+
+    interface ITela
+    {
+        void Pintar();
+    }
+
+    class ClasseBase : IControle, ITela
+    {
+        public void ApertarBotao()
+        {
+
+        }
+
+        public void Pintar()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+
+}
