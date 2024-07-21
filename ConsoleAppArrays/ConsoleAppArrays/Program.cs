babfb669b1196b94286db4dde26276d5b394c3d3
charlespsc <charlesp.sc@outlook.com>
2024-07-21 14:35:14 -0300

    Exemplo Arrays

---
+++ ConsoleAppArrays/ConsoleAppArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleAppArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes;
+            nomes = new string[5];
+
+            nomes[0] = "Joana";
+            nomes[1] = "Joana";
+            nomes[2] = "Joana";
+            nomes[3] = "Joana";
+            nomes[4] = "Joana";
+
+            Console.WriteLine(nomes[1]);
+        }
+    }
+}
